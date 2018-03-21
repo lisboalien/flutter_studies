fb30c992f3d2fef3b437ed189d43dc0002cab021
lisboalien <lisboalien@gmail.com>
2018-03-21 16:54:48 -0300

    Nesting Rows and Columns, this app works better with tablets

---
+++ nesting_rows_columns/README.md
@@ -0,0 +1,8 @@
+# nesting_rows_columns
+
+Nesting rows and columns
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ nesting_rows_columns/lib/main.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var titleText = new Text(
+      'Strawberry Pavlova',
+      style: new TextStyle(
+        fontWeight: FontWeight.w800,
+        letterSpacing: 0.5,
+        fontSize: 30.0,
+      ),
+    );
+
+    var subTitle = new Text(
+      '''Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova. Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.''',
+      style: new TextStyle(
+        fontFamily: 'Georgia',
+        fontSize: 25.0,
+      ),
+    );
+
+    var star = new Icon(Icons.star, color: Colors.black);
+
+    var ratings = new Container(
+      padding: new EdgeInsets.all(20.0),
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          new Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              star,
+              star,
+              star,
+              star,
+              star,
+            ],
+          ),
+          new Text(
+            '170 Reviews',
+            style: new TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20.0,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    var descTextStyle = new TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18.0,
+      height: 2.0,
+    );
+
+    var iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: new Container(
+        padding: new EdgeInsets.all(20.0),
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            new Column(
+              children: [
+                new Icon(Icons.kitchen, color: Colors.green[500]),
+                new Text('PREP:'),
+                new Text('25 MIN'),
+              ],
+            ),
+            new Column(
+              children: [
+                new Icon(Icons.timer, color: Colors.green[500]),
+                new Text('COOK:'),
+                new Text('1 hr'),
+              ],
+            ),
+            new Column(
+              children: [
+                new Icon(Icons.restaurant, color: Colors.green[500]),
+                new Text('FEEDS:'),
+                new Text('4-6'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+
+    var leftColumn = new Container(
+      padding: new EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
+      child: new Column(
+        children: [
+          titleText,
+          subTitle,
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+
+    var mainImage = new Image.asset(
+      'images/pic1.jpg',
+      fit: BoxFit.cover,
+    );
+
+    return new Scaffold(
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: new Container(
+          margin: new EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 30.0),
+          height: 600.0,
+          child: new Card(
+            child: new Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                new Container(
+                  width: 250.0,
+                  child: leftColumn,
+                ),
+                mainImage,
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
