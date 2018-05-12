7874cf91b6d7e65ca7faf697890856075b7d398f
Aline Lisboa <lisboalien@gmail.com>
2018-05-12 18:54:10 -0300

    'All files for cached_network_image.dart'

---
+++ flutter_general_studies/lib/cached_network_image.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final String title = "Cached Image";
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new Center(
+          child: new CachedNetworkImage(
+              placeholder: new CircularProgressIndicator(),
+              imageUrl:
+              "http://hanedanrpg.com/photos/hanedanrpg/26/128718.jpg"),
+        ),
+      ),
+    );
+  }
+}
--- flutter_general_studies/lib/main.dart
+++ flutter_general_studies/lib/main.dart
@@ -1,30 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final String title = "Fade In Image";
+    final String title = "Cached Image";
     return new MaterialApp(
       title: title,
       home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text(title),
-          ),
-          body: new Stack(
-            children: <Widget>[
-              new Center(
-                child: new CircularProgressIndicator(),
-              ),
-              new Center(
-                  child: new FadeInImage.memoryNetwork(
-                      placeholder: kTransparentImage,
-                      image:
-                          "https://www.google.com.br/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png"))
-            ],
-          )),
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new Center(
+          child: new CachedNetworkImage(
+              placeholder: new CircularProgressIndicator(),
+              imageUrl:
+                  "http://hanedanrpg.com/photos/hanedanrpg/26/128718.jpg"),
+        ),
+      ),
     );
   }
 }
