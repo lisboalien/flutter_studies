6055b7d0cb00d566b7b512ae30fb311841430402
Aline Lisboa <lisboalien@gmail.com>
2018-05-12 18:41:31 -0300

    'All files for fade_in_image_with_loading.dart'

---
+++ flutter_general_studies/lib/fade_in_image_with_loading.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final String title = "Fade In Image";
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text(title),
+          ),
+          body: new Stack(
+            children: <Widget>[
+              new Center(
+                child: new CircularProgressIndicator(),
+              ),
+              new Center(
+                  child: new FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image:
+                      "https://www.google.com.br/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png"))
+            ],
+          )),
+    );
+  }
+}
--- flutter_general_studies/lib/main.dart
+++ flutter_general_studies/lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  final appTitle = 'Web Images';
-
   @override
   Widget build(BuildContext context) {
+    final String title = "Fade In Image";
     return new MaterialApp(
-      title: appTitle,
+      title: title,
       home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text(appTitle),
-        ),
-        body: new Center(
-          child: new Image.network(
-              "https://www.google.com.br/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png"),
-        ),
-      ),
+          appBar: new AppBar(
+            title: new Text(title),
+          ),
+          body: new Stack(
+            children: <Widget>[
+              new Center(
+                child: new CircularProgressIndicator(),
+              ),
+              new Center(
+                  child: new FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image:
+                          "https://www.google.com.br/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png"))
+            ],
+          )),
     );
   }
 }
