5bdd973fb5a1aba8b71b06557e938fe0896547b0
Aline Lisboa <lisboalien@gmail.com>
2018-05-12 19:07:20 -0300

    'All files for horizontal_list.dart'

--- flutter_general_studies/lib/main.dart
+++ flutter_general_studies/lib/main.dart
@@ -5,31 +5,43 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final String title = "Basic List";
+    final String title = "Horizontal List";
     return new MaterialApp(
       title: title,
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text(title),
         ),
-        body: new ListView(
-          children: <Widget>[
-            new ListTile(
-              leading: new Icon(Icons.map),
-              title: new Text("Map"),
-            ),
-            new ListTile(
-              leading: new Icon(Icons.photo_album),
-              title: new Text("Photo Album"),
-            ),
-            new ListTile(
-              leading: new Icon(Icons.phone),
-              title: new Text("Phone"),
-            ),
-          ],
+        body: new Container(
+          margin: new EdgeInsets.symmetric(vertical: 20.0),
+          height: 200.0,
+          child: new ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              new Container(
+                width: 200.0,
+                color: Colors.red,
+              ),
+              new Container(
+                width: 200.0,
+                color: Colors.blue,
+              ),
+              new Container(
+                width: 200.0,
+                color: Colors.green,
+              ),
+              new Container(
+                width: 200.0,
+                color: Colors.yellow,
+              ),
+              new Container(
+                width: 200.0,
+                color: Colors.orange,
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
