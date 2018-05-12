e3926e4905a8fd073f1e92eb4d3d453e0ee58e78
Aline Lisboa <lisboalien@gmail.com>
2018-05-12 18:11:52 -0300

    'All files to the drawer_example'

--- flutter_general_studies/lib/main.dart
+++ flutter_general_studies/lib/main.dart
@@ -1,40 +1,57 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new TabBarDemo());
+void main() => runApp(new MyApp());
 
-class TabBarDemo extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  final String appTitle = "Drawer Demo";
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new DefaultTabController(
-          length: 4,
-          child: new Scaffold(
-            appBar: new AppBar(
-              bottom: new TabBar(
-                tabs: <Widget>[
-                  new Tab(
-                    icon: new Icon(Icons.directions_bike),
-                  ),
-                  new Tab(
-                    icon: new Icon(Icons.directions_boat),
-                  ),
-                  new Tab(
-                    icon: new Icon(Icons.directions_bus),
-                  ),
-                  new Tab(
-                    icon: new Icon(Icons.directions_car),
-                  ),
-                ],
-              ),
-              title: new Text("TabBar Demo",),
+      title: appTitle,
+      home: new MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: new Text("My Page!"),
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            new DrawerHeader(
+              child: new Text("Header"),
+              decoration: new BoxDecoration(color: Colors.blue),
             ),
-            body: new TabBarView(children: <Widget>[
-              new Icon(Icons.directions_bike),
-              new Icon(Icons.directions_boat),
-              new Icon(Icons.directions_bus),
-              new Icon(Icons.directions_car),
-            ]),
-          )),
+            new ListTile(
+              title: new Text("Item 1"),
+              onTap: (){
+                // Navigator.pop closes the drawer or returns to the previous page
+                Navigator.pop(context);
+              },
+            ),
+            new ListTile(
+              title: new Text("Item 2"),
+              onTap: (){
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
